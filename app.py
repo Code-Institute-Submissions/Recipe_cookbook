ac2117a682059e9ab247bee70f119bc79b8d24d8
Conor Guider <con.guider@gmail.com>
2019-02-13 17:38:02 +0000

    edit page functions and updates

--- app.py
+++ app.py
@@ -25,6 +25,27 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+    
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_courses = mongo.db.courses.find()
+    return render_template('editrecipe.html', recipe=the_recipe, courses=all_courses)
+    
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id' : ObjectId(recipe_id)},
+    {
+        'recipe_name' : request.form.get('recipe_name'),
+        'course_name' : request.form.get('course_name'),
+        'recipe_description' : request.form.get('recipe_description'),
+        'recipe_ingredients' : request.form.get('recipe_ingredients'),
+        'recipe_instructions' : request.form.get('recipe_instructions'),
+        'cooking_time' : request.form.get('cooking_time')
+    })
+    return redirect(url_for('get_recipes'))
+    
 
 
 
@@ -53,6 +74,7 @@ def insert_recipe():
 #     session.pop('username', None)
 #     return redirect(url_for('login'))
     
+
 if __name__=='__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
